670517cc428111cd2a4bd870a052531f9ab8e986
Diego <diegoxgarsia@hotmail.com>
2018-05-29 02:15:59 +0200

    Añadir funcion menu a main.c
    Se añade la funcion menu() que mostrara las opciones que se pueden realizar en el programa

---
+++ GitHub/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+function menu()
+{
+	printf("MENU");
+	printf("1-SUMAR\n");
+	printf("2-RESTAR\n");
+	printf("3-MULTIPLICAR\n");
+	printf("4-DIVIDIR\n");
+}
+
+void main(void)
+{
+	menu();
+
+}
